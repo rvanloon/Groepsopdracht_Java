e83eee0a3fd2b48c6855b5bf36dedfb764a6fce6
Alain <alain.vandervorst@gmail.com>
2012-10-15 21:29:21 +0200

    Alain

--- src/model/QuizOpdracht.java
+++ src/model/QuizOpdracht.java
@@ -5,9 +5,52 @@ public class QuizOpdracht {
 	/**
 	 * @param args
 	 */
+	
+	private int maxScore;
+	private Opdracht Opdracht;
+	private Quiz Quiz;
+	
+	
+	
+	public int getMaxScore() {
+		return maxScore;
+	}
+
+
+
+	public void setMaxScore(int maxScore) {
+		this.maxScore = maxScore;
+	}
+
+
+
+	public Opdracht getOpdracht() {
+		return Opdracht;
+	}
+
+
+
+	public void setOpdracht(Opdracht opdracht) {
+		Opdracht = opdracht;
+	}
+
+
+
+	public Quiz getQuiz() {
+		return Quiz;
+	}
+
+
+
+	public void setQuiz(Quiz quiz) {
+		Quiz = quiz;
+	}
+
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
