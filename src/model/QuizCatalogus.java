a541b1e3c550e5c534ea502ae5ba1aef29388109
Robrecht Van Loon <robrecht.vanloon@gmail.com>
2012-10-15 20:40:01 +0200

    modelklassen toegevoegd

---
+++ src/model/QuizCatalogus.java
@@ -0,0 +1,13 @@
+package model;
+
+public class QuizCatalogus {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/model/QuizOpdracht.java
@@ -0,0 +1,13 @@
+package model;
+
+public class QuizOpdracht {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
