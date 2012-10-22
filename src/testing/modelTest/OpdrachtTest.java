72225b136524c101399741771cb12aab196239ff
Robrecht Van Loon <robrecht.vanloon@gmail.com>
2012-10-22 20:20:54 +0200

    testing model toegevoegd

---
+++ src/testing/modelTest/OpdrachtTest.java
@@ -0,0 +1,129 @@
+package testing.modelTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class OpdrachtTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetVraag() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetVraag() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetJuisteAntwoord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetJuisteAntwoord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMaxAantalPogingen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMaxAantalPogingen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAntwoordHints() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMaxAntwoordTijd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMaxAntwoordTijd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCategorie() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCategorie() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetQuizzen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAuteur() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAuteur() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDatumRegistratie() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDatumRegistratie() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddAntwoordHint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddQuizopdracht() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsJuisteAntwoord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+}
