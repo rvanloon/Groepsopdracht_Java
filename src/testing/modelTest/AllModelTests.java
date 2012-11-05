19da6553912043a45e11bc5a7993306a293e28b1
Robrecht Van Loon <robrecht.vanloon@gmail.com>
2012-11-05 18:17:22 +0100

    Testsuites toegevoegd

---
+++ src/testing/modelTest/AllModelTests.java
@@ -0,0 +1,14 @@
+package testing.modelTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ LeerlingContainerTest.class, LeerlingTest.class,
+		OpdrachtAntwoordTest.class, OpdrachtCatalogusTest.class,
+		OpdrachtTest.class, QuizCatalogusTest.class, QuizDeelnameTest.class,
+		QuizOpdrachtTest.class, QuizTest.class })
+public class AllModelTests {
+
+}
---
+++ src/testing/utilsTest/AllUtilTests.java
@@ -0,0 +1,11 @@
+package testing.utilsTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ DatumGregorianTest.class, DatumTest.class })
+public class AllUtilTests {
+
+}
