3daa057643b302a28584467bd3e7396309fda5bc
Robrecht Van Loon <robrecht.vanloon@gmail.com>
2012-11-12 20:04:15 +0100

    y

---
+++ src/model/FileContainer.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package model;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Formatter;
+
+/**
+ * @author rvanloon
+ * 
+ */
+public abstract class FileContainer {
+
+	public void lezen() {
+		try {
+			File file = new File(getFile());
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			String lijn = null;
+			while (!(lijn = reader.readLine()).equals("EINDE")) {
+				toevoegenLijn(lijn);
+			}
+			reader.close();
+		} catch (Exception ex) {
+		}
+	}
+
+	public void Schrijven(ArrayList<String> lijnen) throws Exception {
+		Formatter schrijver = null;
+		File file = new File(getFile());
+
+		try {
+			// open file
+			schrijver = new Formatter(file);
+
+			// records schrijven
+			for (String s : lijnen) {
+				schrijver.format("%s%n", s);
+			}
+		} catch (SecurityException ex) {
+			throw new Exception("Geen schrijftoegeang tot bestand");
+		} catch (FileNotFoundException ex) {
+			throw new Exception("Kan bestand niet openen");
+		} finally {
+			// sluit schrijver
+			if (schrijver != null) {
+				schrijver.close();
+			}
+		}
+	}
+
+	public abstract String getFile();
+
+	public abstract void toevoegenLijn(String lijn);
+
+}
---
+++ src/model/PersisteerbaarAlsTekst.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author rvanloon
+ *
+ */
+public interface PersisteerbaarAlsTekst {
+	
+	void maakObjectVanLijn(String[]velden);
+	
+	String MaakLijnVanObject(Object o);
+}
